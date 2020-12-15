7431753c2669f08dc79e272050bbd6bec8ec5179
Parham-Baghbanbashi204 <parham.baghbanbashi204@gmail.com>
2020-12-15 15:45:21 -0500

    Fixed OpMode

--- TeamCode/src/main/java/com/team16488/compoonents/DriveStraight.java
+++ TeamCode/src/main/java/com/team16488/compoonents/DriveStraight.java
@@ -29,6 +29,7 @@ public class DriveStraight {
         this.robot.RearLeftMotor.setPower(leftPower);
     }
 
+
     // Arcade Mode uses yAxis(traditionally left stick) to go forward, and xAxis
     // (traditionally right stick) to turn.
     public void driveArcade(double xAxis, double yAxis){
